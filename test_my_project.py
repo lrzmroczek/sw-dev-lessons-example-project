be5adde83506ec49953669ad0af9e70949815de8
lrzmroczek <85950935+lrzmroczek@users.noreply.github.com>
2024-02-25 13:58:30 -0500

    adjust tests for median

--- test_my_project.py
+++ test_my_project.py
@@ -4,8 +4,8 @@ def test_average():
     assert get_average([1]) == 1
     assert get_average([2, 1]) == 1.5
     assert get_average([10, 50, 30]) == 30
-    assert get_average([0, 0, 0, 4]) == 1
+    assert get_average([0, 0, 0, 4]) == 0
 
 
 def test_data_set():
-    assert do_algorithm([1, 2, 2, 5, 5]) == 3
+    assert do_algorithm([1, 2, 2, 5, 5]) == 2
